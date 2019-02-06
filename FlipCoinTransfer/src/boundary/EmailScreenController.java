7dc517ea18506c9b4c4fb71d60219c74d3bc3797
ofrii <47346476+ofrii@users.noreply.github.com>
2019-02-06 21:04:02 +0200

    EmailScreen

---
+++ FlipCoinTransfer/src/boundary/EmailScreenController.java
@@ -0,0 +1,136 @@
+package boundary;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class EmailScreenController {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private HBox topBorder;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private ImageView searchIcon;
+
+    @FXML
+    private TextField searchText;
+
+    @FXML
+    private ImageView mailIcon;
+
+    @FXML
+    private ImageView settingsIcon;
+
+    @FXML
+    private ImageView logoutIcon;
+
+    @FXML
+    private VBox bottom;
+
+    @FXML
+    private ImageView line;
+
+    @FXML
+    private HBox hbox;
+
+    @FXML
+    private Label networkStatus;
+
+    @FXML
+    private ImageView homeIcon;
+
+    @FXML
+    private VBox menu;
+
+    @FXML
+    private ImageView products;
+
+    @FXML
+    private ImageView transactions;
+
+    @FXML
+    private ImageView wallets;
+
+    @FXML
+    private ImageView arrow1;
+
+    @FXML
+    private ImageView arrow2;
+
+    @FXML
+    private ImageView arrow3;
+
+    @FXML
+    private TableView<?> inbox;
+
+    @FXML
+    private TextField dateArea;
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    void logOut(MouseEvent event) {
+
+    }
+
+    @FXML
+    void mailsScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void productsScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void searchProducts(MouseEvent event) {
+
+    }
+
+    @FXML
+    void settingsScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void show1(DragEvent event) {
+
+    }
+
+    @FXML
+    void show2(DragEvent event) {
+
+    }
+
+    @FXML
+    void show3(DragEvent event) {
+
+    }
+
+    @FXML
+    void transactionsScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void walletsScreen(MouseEvent event) {
+
+    }
+
+}

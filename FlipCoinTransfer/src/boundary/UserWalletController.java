de88088ce69e6e132e41016c6a790f020b500f45
ofrii <47346476+ofrii@users.noreply.github.com>
2019-02-06 22:22:08 +0200

    Wallets - unfinished

---
+++ FlipCoinTransfer/src/boundary/UserWalletController.java
@@ -0,0 +1,100 @@
+package boundary;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class UserWalletController {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private VBox menu;
+
+    @FXML
+    private ImageView products;
+
+    @FXML
+    private ImageView transactions;
+
+    @FXML
+    private ImageView wallets;
+
+    @FXML
+    private HBox topBorder;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private ImageView searchIcon;
+
+    @FXML
+    private TextField searchText;
+
+    @FXML
+    private ImageView mailIcon;
+
+    @FXML
+    private ImageView settingsIcon;
+
+    @FXML
+    private ImageView logoutIcon;
+
+    @FXML
+    private VBox bottom;
+
+    @FXML
+    private ImageView line;
+
+    @FXML
+    private HBox hbox;
+
+    @FXML
+    private Label networkStatus;
+
+    @FXML
+    private ImageView homeIcon;
+
+    @FXML
+    void logOut(MouseEvent event) {
+
+    }
+
+    @FXML
+    void mailsScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void productsScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void searchProducts(MouseEvent event) {
+
+    }
+
+    @FXML
+    void settingsScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void transactionsScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void walletsScreen(MouseEvent event) {
+
+    }
+
+}

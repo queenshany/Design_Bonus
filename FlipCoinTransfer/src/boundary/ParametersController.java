02f6305284db0c4f6a449974c5a2b8389903027d
ofrii <47346476+ofrii@users.noreply.github.com>
2019-02-07 22:50:28 +0200

    Parameters Screen

---
+++ FlipCoinTransfer/src/boundary/ParametersController.java
@@ -0,0 +1,212 @@
+package boundary;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class ParametersController {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private HBox topBorder;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private Label networkStatus;
+
+    @FXML
+    private ImageView logoutIcon;
+
+    @FXML
+    private VBox bottom;
+
+    @FXML
+    private ImageView line;
+
+    @FXML
+    private HBox hbox;
+
+    @FXML
+    private ImageView homeIcon;
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private ImageView createRec;
+
+    @FXML
+    private ImageView viewRec;
+
+    @FXML
+    private ImageView transRpt;
+
+    @FXML
+    private ImageView usersRpt;
+
+    @FXML
+    private ImageView categories;
+
+    @FXML
+    private ImageView parameters;
+
+    @FXML
+    private VBox vbox1;
+
+    @FXML
+    private HBox hbox2;
+
+    @FXML
+    private VBox vbox2;
+
+    @FXML
+    private Label lab1;
+
+    @FXML
+    private TextField transMinSize;
+
+    @FXML
+    private HBox h3;
+
+    @FXML
+    private VBox v3;
+
+    @FXML
+    private Label lab2;
+
+    @FXML
+    private TextField transMaxSize;
+
+    @FXML
+    private HBox h4;
+
+    @FXML
+    private VBox v4;
+
+    @FXML
+    private Label lab3;
+
+    @FXML
+    private TextField sizeForExpansion;
+
+    @FXML
+    private HBox h6;
+
+    @FXML
+    private VBox v6;
+
+    @FXML
+    private Label lab4;
+
+    @FXML
+    private TextField priceForExpansion;
+
+    @FXML
+    private VBox v7;
+
+    @FXML
+    private HBox h7;
+
+    @FXML
+    private VBox v8;
+
+    @FXML
+    private Label lab5;
+
+    @FXML
+    private TextField discountPrecent;
+
+    @FXML
+    private HBox h8;
+
+    @FXML
+    private VBox v9;
+
+    @FXML
+    private Label lab6;
+
+    @FXML
+    private TextField priceForDiscount;
+
+    @FXML
+    private HBox h9;
+
+    @FXML
+    private VBox v10;
+
+    @FXML
+    private Label lab7;
+
+    @FXML
+    private TextField transSizeFree;
+
+    @FXML
+    private HBox h10;
+
+    @FXML
+    private VBox v11;
+
+    @FXML
+    private Label lab8;
+
+    @FXML
+    private TextField maxDiscount;
+
+    @FXML
+    private HBox h11;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    void allRecommendations(MouseEvent event) {
+
+    }
+
+    @FXML
+    void editVirsion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void generateTransReport(MouseEvent event) {
+
+    }
+
+    @FXML
+    void generateUsersReport(MouseEvent event) {
+
+    }
+
+    @FXML
+    void logOut(MouseEvent event) {
+
+    }
+
+    @FXML
+    void manageCategories(MouseEvent event) {
+
+    }
+
+    @FXML
+    void manageParameters(MouseEvent event) {
+
+    }
+
+    @FXML
+    void newRecommendation(MouseEvent event) {
+
+    }
+
+}

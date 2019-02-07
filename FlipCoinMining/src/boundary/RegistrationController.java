0b217b9e5250957f9aaaa6e8c02d628234d9102e
ofrii <47346476+ofrii@users.noreply.github.com>
2019-02-07 16:49:40 +0200

    RegisterScreen

--- FlipCoinMining/src/boundary/RegistrationController.java
+++ FlipCoinMining/src/boundary/RegistrationController.java
@@ -1,5 +1,131 @@
 package boundary;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 public class RegistrationController {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private VBox topBorder;
+
+    @FXML
+    private ImageView registerTitle;
+
+    @FXML
+    private Label clickHere;
+
+    @FXML
+    private VBox centerBorder;
+
+    @FXML
+    private HBox HBox1;
+
+    @FXML
+    private Label username;
+
+    @FXML
+    private TextField usernameText;
+
+    @FXML
+    private Label usernameLable;
+
+    @FXML
+    private HBox HBox2;
+
+    @FXML
+    private Label password;
+
+    @FXML
+    private PasswordField passwordText;
+
+    @FXML
+    private Label passwordLable;
+
+    @FXML
+    private HBox HBox3;
+
+    @FXML
+    private Label verify;
+
+    @FXML
+    private PasswordField verifyText;
+
+    @FXML
+    private Label verifyLable;
+
+    @FXML
+    private HBox Hbox51;
+
+    @FXML
+    private Label email1;
+
+    @FXML
+    private TextField emailText1;
+
+    @FXML
+    private Label emailLable1;
+
+    @FXML
+    private HBox HBox11;
+
+    @FXML
+    private Label username1;
+
+    @FXML
+    private TextField usernameText1;
+
+    @FXML
+    private Label usernameLable1;
+
+    @FXML
+    private HBox HBox4;
+
+    @FXML
+    private Label phone;
+
+    @FXML
+    private ComboBox<?> combo;
+
+    @FXML
+    private TextField phoneText;
+
+    @FXML
+    private Label phoneLable;
+
+    @FXML
+    private HBox Hbox5;
+
+    @FXML
+    private Label email;
+
+    @FXML
+    private TextField emailText;
+
+    @FXML
+    private Label emailLable;
+
+    @FXML
+    private Label registerLable;
+
+    @FXML
+    void newAccount(MouseEvent event) {
+
+    }
+
+    @FXML
+    void openCompanyField(MouseEvent event) {
+
+    }
 
 }

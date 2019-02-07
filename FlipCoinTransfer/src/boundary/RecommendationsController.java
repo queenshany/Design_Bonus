917b9436ff33a8056ffc583f300ce8586f845ab8
ofrii <47346476+ofrii@users.noreply.github.com>
2019-02-07 20:45:07 +0200

    ViewRecommendations

---
+++ FlipCoinTransfer/src/boundary/RecommendationsController.java
@@ -0,0 +1,97 @@
+package boundary;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class RecommendationsController {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private HBox topBorder;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private Label networkStatus;
+
+    @FXML
+    private ImageView logoutIcon;
+
+    @FXML
+    private VBox bottom;
+
+    @FXML
+    private ImageView line;
+
+    @FXML
+    private HBox hbox;
+
+    @FXML
+    private ImageView homeIcon;
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    private ImageView createRec;
+
+    @FXML
+    private ImageView viewRec;
+
+    @FXML
+    private ImageView transRpt;
+
+    @FXML
+    private ImageView usersRpt;
+
+    @FXML
+    private ImageView categories;
+
+    @FXML
+    private ImageView parameters;
+
+    @FXML
+    void allRecommendations(MouseEvent event) {
+
+    }
+
+    @FXML
+    void generateTransReport(MouseEvent event) {
+
+    }
+
+    @FXML
+    void generateUsersReport(MouseEvent event) {
+
+    }
+
+    @FXML
+    void logOut(MouseEvent event) {
+
+    }
+
+    @FXML
+    void manageCategories(MouseEvent event) {
+
+    }
+
+    @FXML
+    void manageParameters(MouseEvent event) {
+
+    }
+
+    @FXML
+    void newRecommendation(MouseEvent event) {
+
+    }
+
+}

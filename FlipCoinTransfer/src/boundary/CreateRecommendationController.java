6cfc437f883c738d4ec74642fe9a34b80efe526a
ofrii <47346476+ofrii@users.noreply.github.com>
2019-02-07 21:39:56 +0200

    CreateRecommendation

---
+++ FlipCoinTransfer/src/boundary/CreateRecommendationController.java
@@ -0,0 +1,145 @@
+package boundary;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class CreateRecommendationController {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private HBox topBorder;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private Label networkStatus;
+
+    @FXML
+    private ImageView logoutIcon;
+
+    @FXML
+    private VBox bottom;
+
+    @FXML
+    private ImageView line;
+
+    @FXML
+    private HBox hbox;
+
+    @FXML
+    private ImageView homeIcon;
+
+    @FXML
+    private ImageView createRec;
+
+    @FXML
+    private ImageView viewRec;
+
+    @FXML
+    private ImageView transRpt;
+
+    @FXML
+    private ImageView usersRpt;
+
+    @FXML
+    private ImageView categories;
+
+    @FXML
+    private ImageView parameters;
+
+    @FXML
+    private Label commisionLable;
+
+    @FXML
+    private Label probabilityLable;
+
+    @FXML
+    private TextField commision;
+
+    @FXML
+    private TextField probability;
+
+    @FXML
+    private Button newButton;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private Label usernameLable;
+
+    @FXML
+    private Label levelOfCommitment;
+
+    @FXML
+    private ComboBox<?> usernameCombo;
+
+    @FXML
+    private ComboBox<?> levelCombo;
+
+    @FXML
+    private Button sendButton;
+
+    @FXML
+    void allRecommendations(MouseEvent event) {
+
+    }
+
+    @FXML
+    void generateTransReport(MouseEvent event) {
+
+    }
+
+    @FXML
+    void generateUsersReport(MouseEvent event) {
+
+    }
+
+    @FXML
+    void logOut(MouseEvent event) {
+
+    }
+
+    @FXML
+    void manageCategories(MouseEvent event) {
+
+    }
+
+    @FXML
+    void manageParameters(MouseEvent event) {
+
+    }
+
+    @FXML
+    void newMethod(ActionEvent event) {
+
+    }
+
+    @FXML
+    void newRecommendation(MouseEvent event) {
+
+    }
+
+    @FXML
+    void saveMethod(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sendMethod(ActionEvent event) {
+
+    }
+
+}

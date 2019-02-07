51caef3b78724e03bb53502176c6d875627b88e3
ofrii <47346476+ofrii@users.noreply.github.com>
2019-02-07 13:54:05 +0200

    Settings Screen

---
+++ FlipCoinTransfer/src/boundary/SettingsController.java
@@ -0,0 +1,138 @@
+package boundary;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class SettingsController {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private VBox menu;
+
+    @FXML
+    private ImageView products;
+
+    @FXML
+    private ImageView transactions;
+
+    @FXML
+    private ImageView wallets;
+
+    @FXML
+    private HBox topBorder;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private ImageView searchIcon;
+
+    @FXML
+    private TextField searchText;
+
+    @FXML
+    private ImageView mailIcon;
+
+    @FXML
+    private ImageView settingsIcon;
+
+    @FXML
+    private ImageView logoutIcon;
+
+    @FXML
+    private VBox bottom;
+
+    @FXML
+    private ImageView line;
+
+    @FXML
+    private HBox hbox;
+
+    @FXML
+    private Label networkStatus;
+
+    @FXML
+    private ImageView homeIcon;
+
+    @FXML
+    private HBox HBox4;
+
+    @FXML
+    private Label phone;
+
+    @FXML
+    private ComboBox<?> combo;
+
+    @FXML
+    private TextField phoneText;
+
+    @FXML
+    private Label phoneLable;
+
+    @FXML
+    private HBox Hbox5;
+
+    @FXML
+    private Label email;
+
+    @FXML
+    private TextField emailText;
+
+    @FXML
+    private Label emailLable;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    void logOut(MouseEvent event) {
+
+    }
+
+    @FXML
+    void mailsScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void productsScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void searchProducts(MouseEvent event) {
+
+    }
+
+    @FXML
+    void settingsScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void transactionsScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void updateField(ActionEvent event) {
+
+    }
+
+    @FXML
+    void walletsScreen(MouseEvent event) {
+
+    }
+
+}

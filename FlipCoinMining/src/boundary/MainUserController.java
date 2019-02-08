9b22e2b9311ffce6b92b1da2ce60a2e6ca36afbb
ofrii <47346476+ofrii@users.noreply.github.com>
2019-02-08 14:36:51 +0200

    Mining - Main - User

---
+++ FlipCoinMining/src/boundary/MainUserController.java
@@ -0,0 +1,92 @@
+package boundary;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class MainUserScreenController {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private VBox menu;
+
+    @FXML
+    private ImageView lotteries;
+
+    @FXML
+    private ImageView riddles;
+
+    @FXML
+    private ImageView blocks;
+
+    @FXML
+    private ImageView miners;
+
+    @FXML
+    private ImageView report;
+
+    @FXML
+    private HBox topBorder;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private ImageView mailIcon;
+
+    @FXML
+    private ImageView logoutIcon;
+
+    @FXML
+    private VBox bottom;
+
+    @FXML
+    private ImageView line;
+
+    @FXML
+    private HBox hbox;
+
+    @FXML
+    private ImageView homeIcon;
+
+    @FXML
+    void logOut(MouseEvent event) {
+
+    }
+
+    @FXML
+    void lotteriesScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void mailsScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void manageBlocks(MouseEvent event) {
+
+    }
+
+    @FXML
+    void minersScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void riddlesScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void watchReport(MouseEvent event) {
+
+    }
+
+}

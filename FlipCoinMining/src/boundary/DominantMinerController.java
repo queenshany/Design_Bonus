f95dc77ad4efd81548497b519666ee925ec434f4
ofrii <47346476+ofrii@users.noreply.github.com>
2019-02-08 16:36:37 +0200

    Dominant Report

---
+++ FlipCoinMining/src/boundary/DominantMinerController.java
@@ -0,0 +1,112 @@
+package boundary;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class DominantMinerController {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private VBox menu;
+
+    @FXML
+    private ImageView importJ;
+
+    @FXML
+    private ImageView exportE;
+
+    @FXML
+    private ImageView management;
+
+    @FXML
+    private ImageView report;
+
+    @FXML
+    private HBox topBorder;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private ImageView mailIcon;
+
+    @FXML
+    private ImageView logoutIcon;
+
+    @FXML
+    private VBox bottom;
+
+    @FXML
+    private ImageView line;
+
+    @FXML
+    private HBox hbox;
+
+    @FXML
+    private ImageView homeIcon;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private Label start;
+
+    @FXML
+    private DatePicker startDate;
+
+    @FXML
+    private Label end;
+
+    @FXML
+    private DatePicker endDate;
+
+    @FXML
+    private Label generateLable;
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    private ImageView V;
+
+    @FXML
+    void exportXML(MouseEvent event) {
+
+    }
+
+    @FXML
+    void generateReport(MouseEvent event) {
+
+    }
+
+    @FXML
+    void importJSON(MouseEvent event) {
+
+    }
+
+    @FXML
+    void logOut(MouseEvent event) {
+
+    }
+
+    @FXML
+    void mailsScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void manageScreen(MouseEvent event) {
+
+    }
+
+}

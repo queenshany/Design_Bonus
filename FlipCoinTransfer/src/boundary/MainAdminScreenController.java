ab14d9f36b86295b744d0b8fcc6c5e70c129c570
ofrii <47346476+ofrii@users.noreply.github.com>
2019-02-08 10:35:19 +0200

    Main Screen - Admin

---
+++ FlipCoinTransfer/src/boundary/MainAdminScreenController.java
@@ -0,0 +1,93 @@
+package boundary;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class MainAdminScreenController {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private HBox topBorder;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private Label networkStatus;
+
+    @FXML
+    private ImageView logoutIcon;
+
+    @FXML
+    private VBox bottom;
+
+    @FXML
+    private ImageView line;
+
+    @FXML
+    private HBox hbox;
+
+    @FXML
+    private ImageView homeIcon;
+
+    @FXML
+    private ImageView importXML;
+
+    @FXML
+    private ImageView expJson;
+
+    @FXML
+    private ImageView viewDetails;
+
+    @FXML
+    private ImageView employeesManag;
+
+    @FXML
+    private ImageView categories;
+
+    @FXML
+    private ImageView parameters;
+
+    @FXML
+    void allDetails(MouseEvent event) {
+
+    }
+
+    @FXML
+    void exportJson(MouseEvent event) {
+
+    }
+
+    @FXML
+    void importXML(MouseEvent event) {
+
+    }
+
+    @FXML
+    void logOut(MouseEvent event) {
+
+    }
+
+    @FXML
+    void manageCategories(MouseEvent event) {
+
+    }
+
+    @FXML
+    void manageEmployees(MouseEvent event) {
+
+    }
+
+    @FXML
+    void manageParameters(MouseEvent event) {
+
+    }
+
+}

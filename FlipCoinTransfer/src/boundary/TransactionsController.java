4a9d0665f5dc93347e169a18b8462d90f39660ce
ofrii <47346476+ofrii@users.noreply.github.com>
2019-02-08 22:31:45 +0200

    Transactions Screen

---
+++ FlipCoinTransfer/src/boundary/TransactionsController.java
@@ -0,0 +1,174 @@
+package boundary;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class TransactionsController {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private VBox menu;
+
+    @FXML
+    private ImageView products;
+
+    @FXML
+    private ImageView transactions;
+
+    @FXML
+    private ImageView wallets;
+
+    @FXML
+    private HBox topBorder;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private ImageView searchIcon;
+
+    @FXML
+    private TextField searchText;
+
+    @FXML
+    private ImageView mailIcon;
+
+    @FXML
+    private ImageView settingsIcon;
+
+    @FXML
+    private ImageView logoutIcon;
+
+    @FXML
+    private VBox bottom;
+
+    @FXML
+    private ImageView line;
+
+    @FXML
+    private HBox hbox;
+
+    @FXML
+    private Label networkStatus;
+
+    @FXML
+    private ImageView homeIcon;
+
+    @FXML
+    private TabPane tabPane;
+
+    @FXML
+    private Tab newPT;
+
+    @FXML
+    private AnchorPane pane1;
+
+    @FXML
+    private ComboBox<?> usersCombo;
+
+    @FXML
+    private ComboBox<?> productsCombo;
+
+    @FXML
+    private TextField amountText;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    private Label lable;
+
+    @FXML
+    private Label price;
+
+    @FXML
+    private Label btc;
+
+    @FXML
+    private Button createButton;
+
+    @FXML
+    private Button delettButton;
+
+    @FXML
+    private Label errorMassage;
+
+    @FXML
+    void NewPayTransaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addToTable(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logOut(MouseEvent event) {
+
+    }
+
+    @FXML
+    void mailsScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void productsScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void searchProducts(MouseEvent event) {
+
+    }
+
+    @FXML
+    void settingsScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void switch1(MouseEvent event) {
+
+    }
+
+    @FXML
+    void switch2(MouseEvent event) {
+
+    }
+
+    @FXML
+    void switch3(MouseEvent event) {
+
+    }
+
+    @FXML
+    void transactionsScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void walletsScreen(MouseEvent event) {
+
+    }
+
+}

2485544b7960d5ff041f24700bbb7cabcdc0e289
ofrii <47346476+ofrii@users.noreply.github.com>
2019-02-09 10:36:24 +0200

    Lotteries Screen

---
+++ FlipCoinMining/src/boundary/LotteriesScreenController.java
@@ -0,0 +1,111 @@
+package boundary;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class LotteriesScreenController {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private VBox menu;
+
+    @FXML
+    private ImageView lotteries;
+
+    @FXML
+    private ImageView riddles;
+
+    @FXML
+    private ImageView blocks;
+
+    @FXML
+    private ImageView miners;
+
+    @FXML
+    private ImageView report;
+
+    @FXML
+    private HBox topBorder;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private ImageView mailIcon;
+
+    @FXML
+    private ImageView logoutIcon;
+
+    @FXML
+    private VBox bottom;
+
+    @FXML
+    private ImageView line;
+
+    @FXML
+    private HBox hbox;
+
+    @FXML
+    private ImageView homeIcon;
+
+    @FXML
+    private ComboBox<?> lorretycombo;
+
+    @FXML
+    private ImageView enterButton;
+
+    @FXML
+    private Label lable;
+
+    @FXML
+    private Label errorMassage;
+
+    @FXML
+    void addUserToLottery(MouseEvent event) {
+
+    }
+
+    @FXML
+    void logOut(MouseEvent event) {
+
+    }
+
+    @FXML
+    void lotteriesScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void mailsScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void manageBlocks(MouseEvent event) {
+
+    }
+
+    @FXML
+    void minersScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void riddlesScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void watchReport(MouseEvent event) {
+
+    }
+
+}

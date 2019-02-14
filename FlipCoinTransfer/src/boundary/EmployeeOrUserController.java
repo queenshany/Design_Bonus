4d2ae21adfab15b00c57233847904f4448ed79e2
ofrii <47346476+ofrii@users.noreply.github.com>
2019-02-14 20:49:20 +0200

    Employee or User

---
+++ FlipCoinTransfer/src/boundary/EmployeeOrUserController.java
@@ -0,0 +1,36 @@
+package boundary;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class EmployeeOrUserController {
+
+    @FXML
+    private Label l1;
+
+    @FXML
+    private Label l2;
+
+    @FXML
+    private Button userButton;
+
+    @FXML
+    private Button employeeButton;
+
+    @FXML
+    private ImageView line;
+
+    @FXML
+    void logasEmployee(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logasUser(ActionEvent event) {
+
+    }
+
+}

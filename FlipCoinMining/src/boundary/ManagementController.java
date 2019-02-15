c213bb46d9bd847358635402ad679968da706333
ofrii <47346476+ofrii@users.noreply.github.com>
2019-02-15 12:58:30 +0200

    Managment Screen

--- FlipCoinMining/src/boundary/ManagementController.java
+++ FlipCoinMining/src/boundary/ManagementController.java
@@ -3,8 +3,10 @@ package boundary;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -43,7 +45,7 @@ public class ManagementController {
     private TabPane tabPane;
 
     @FXML
-    private Tab allUsers;
+    private Tab riddles;
 
     @FXML
     private AnchorPane p1;
@@ -64,25 +66,94 @@ public class ManagementController {
     private AnchorPane p2;
 
     @FXML
-    private TableView<?> t2;
+    private TableView<?> lotteriesTable;
+
+    @FXML
+    private TableColumn<?, ?> lotteryNum;
+
+    @FXML
+    private TableColumn<?, ?> lotteryDate;
+
+    @FXML
+    private TableColumn<?, ?> maxParticipants;
+
+    @FXML
+    private TableColumn<?, ?> numOfWinners;
+
+    @FXML
+    private TableColumn<?, ?> numOfBonuses;
 
     @FXML
     private Button addL;
 
     @FXML
-    private Tab allTransactions;
+    private Tab bonuses;
 
     @FXML
     private AnchorPane p3;
 
     @FXML
-    private TableView<?> t3;
+    private TableView<?> bonusesTable;
+
+    @FXML
+    private TableColumn<?, ?> bonusNum;
+
+    @FXML
+    private TableColumn<?, ?> description;
 
     @FXML
     private Button addB;
 
     @FXML
     private Button editB;
+
+    @FXML
+    private Tab levels;
+
+    @FXML
+    private AnchorPane p31;
+
+    @FXML
+    private TableView<?> levelsTable;
+
+    @FXML
+    private TableColumn<?, ?> levelCode;
+
+    @FXML
+    private TableColumn<?, ?> levelName;
+
+    @FXML
+    private TableColumn<?, ?> difficultyLevel;
+
+    @FXML
+    private TableColumn<?, ?> blockSize;
+
+    @FXML
+    private Button addB1;
+
+    @FXML
+    private Button editB1;
+
+    @FXML
+    private TableView<?> minersTable;
+
+    @FXML
+    private AnchorPane p32;
+
+    @FXML
+    private ScrollPane scrollMiners;
+
+    @FXML
+    private TableColumn<?, ?> uniqueAddress;
+
+    @FXML
+    private TableColumn<?, ?> MinerName;
+
+    @FXML
+    private TableColumn<?, ?> Email;
+
+    @FXML
+    private TableColumn<?, ?> DigitalProfit;
 
     @FXML
     void addBonus(ActionEvent event) {
@@ -96,6 +167,26 @@ public class ManagementController {
 
     @FXML
     void addRiddle(ActionEvent event) {
+
+    }
+
+    @FXML
+    void chosenBonus(MouseEvent event) {
+
+    }
+
+    @FXML
+    void chosenLevel(MouseEvent event) {
+
+    }
+
+    @FXML
+    void chosenLottery(MouseEvent event) {
+
+    }
+
+    @FXML
+    void chosenMiner(MouseEvent event) {
 
     }
 

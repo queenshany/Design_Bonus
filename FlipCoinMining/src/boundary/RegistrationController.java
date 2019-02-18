09e343e6a430b8408c831d09683987d76043f3c7
queenshany <44135472+queenshany@users.noreply.github.com>
2019-02-18 10:50:33 +0200

    registration valid in mining

--- FlipCoinMining/src/boundary/RegistrationController.java
+++ FlipCoinMining/src/boundary/RegistrationController.java
@@ -4,15 +4,18 @@ import entity.Consts;
 import entity.Miner;
 import entity.MinerCompany;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import utils.E_Phone;
 
@@ -90,9 +93,9 @@ public class RegistrationController {
 	@FXML
 	private TextField usernameText1;
 
-    @FXML
-    private TextField usernameText11;
-    
+	@FXML
+	private TextField usernameText11;
+
 	@FXML
 	private Label usernameLable1;
 
@@ -127,67 +130,131 @@ public class RegistrationController {
 	private Label registerLable;
 
 	protected static int flag = 0;
-	
-	 public void initialize() {
-		 setPhoneCombo();
-	 }
-	 
-	 private void setPhoneCombo() {
-			combo.getItems().setAll(E_Phone.values());
-			combo.getSelectionModel().select(0);
-			System.out.println(combo.getSelectionModel().getSelectedItem());
-		}
-	 
+
+	public void initialize() {
+		setPhoneCombo();
+	}
+
+	private void setPhoneCombo() {
+		combo.getItems().setAll(E_Phone.values());
+		combo.getSelectionModel().select(0);
+		System.out.println(combo.getSelectionModel().getSelectedItem());
+	}
+
 	@FXML
 	void newAccount(MouseEvent event) {
-			if (!passwordText.getText().equals(verifyText.getText())) {
-				passwordLable.setText("Please try again");
+
+		//Miner miner = control.MinerLogic.getInstance().getMiners().get(0);
+		if (control.Validation.validName(usernameText.getText())) {
+			usernameLable.setVisible(false);
+
+			if (passwordText.getText() != null){
+				passwordLable.setVisible(false);
+
+				if (!passwordText.getText().isEmpty()) {
+					passwordLable.setVisible(false);
+
+					if (passwordText.getText().equals(verifyText.getText())) {
+						passwordLable.setVisible(false);
+						if (control.Validation.validEmailAddress(emailText1.getText())) {
+							emailLable1.setVisible(false);
+
+							String uniqueAddress;
+							do {
+								uniqueAddress = control.SysData.getInstance().generateRandomStrings(Consts.MINER_ADDRESS_LENGTH);
+							}
+							while (control.MinerLogic.getInstance().getMiners().contains(new Miner(uniqueAddress)));
+
+							if (flag == 1) {
+
+								// contact first name
+								if (control.Validation.validName(usernameText1.getText())) {
+									usernameLable1.setVisible(false);
+
+									// contact last name
+									if (control.Validation.validName(usernameText11.getText())) {
+										usernameLable1.setVisible(false);
+
+										if (combo.getSelectionModel().getSelectedItem() != null) {
+											phoneLable.setVisible(false);
+
+											if (control.Validation.validPhone(phoneText.getText())){
+												phoneLable.setVisible(false);
+
+												if (control.Validation.validEmailAddress(emailText.getText())){
+													emailLable.setVisible(false);
+
+													MinerCompany miner = new MinerCompany(uniqueAddress,
+															usernameText.getText(),
+															passwordText.getText(),
+															emailText1.getText(), 
+															0,
+															usernameText1.getText(),
+															usernameText11.getText(),
+															combo.getSelectionModel().getSelectedItem() + "-" + phoneText.getText(),
+															emailText.getText());
+													control.MinerLogic.getInstance().insertMinerCompany(miner);
+													Alert alert = new Alert(AlertType.CONFIRMATION);
+													alert.setTitle("Congrats!");
+													alert.setContentText("You have registered Successfully. +\n"
+															+"Your username is: " + uniqueAddress +" .\nWe recommend you write it down, as it will be used to use the system");
+													alert.initModality(Modality.APPLICATION_MODAL);
+													alert.showAndWait();
+													closeWindow();
+													ViewLogic.newLoginWindow();
+												}else {
+													emailLable.setVisible(true);
+													emailLable.setText("Email is invalid.");
+												}
+											}else {
+												phoneLable.setVisible(true);
+												phoneLable.setText("Phone is invalid.");
+											}
+										}else {
+											phoneLable.setVisible(true);
+											phoneLable.setText("Please select an area code.");
+										}
+									}else {
+										usernameLable1.setVisible(true);
+										usernameLable1.setText("Last name is invalid.");
+									}
+								}else {
+									usernameLable1.setVisible(true);
+									usernameLable1.setText("Last name is invalid.");
+								}
+							}
+							else {
+								Miner miner = new Miner(uniqueAddress, usernameText.getText(),passwordText.getText(),emailText1.getText(), 0);
+								control.MinerLogic.getInstance().insertMiner(miner);
+								Alert alert = new Alert(AlertType.CONFIRMATION);
+								alert.setTitle("Congrats!");
+								alert.setContentText("You have registered Successfully. +\n"
+										+"Your username is: " + uniqueAddress +" .\nWe recommend you write it down, as it will be used to use the system");
+								alert.initModality(Modality.APPLICATION_MODAL);
+								alert.showAndWait();
+								closeWindow();
+								ViewLogic.newLoginWindow();
+							}
+						}else {
+							emailLable1.setVisible(true);
+							emailLable1.setText("Email is invalid.");
+						}
+					}else {
+						passwordLable.setText("Please try again.");
+						passwordLable.setVisible(true);
+					}
+				}else {
+					passwordLable.setText("Please enter a password.");
+					passwordLable.setVisible(true);
+				}
+			}else {
+				passwordLable.setText("Please enter a password.");
 				passwordLable.setVisible(true);
 			}
-			else {
-			Miner miner = control.MinerLogic.getInstance().getMiners().get(0);
-			miner.setMinerName(usernameText.getText());
-			miner.setDigitalProfit(0);
-			miner.setEmail(emailText.getText());
-			miner.setPassword(passwordText.getText());
-			
-			String uniqueAddress;
-			do {
-				uniqueAddress = control.SysData.getInstance().generateRandomStrings(Consts.MINER_ADDRESS_LENGTH);
-			}
-			while (control.MinerLogic.getInstance().getMiners().contains(new Miner(uniqueAddress)));
-			
-			miner.setUniqueAddress(uniqueAddress);
-			
-			control.MinerLogic.getInstance().insertMiner(miner);
-			
-			if (flag==1) {
-				MinerCompany comp = control.MinerLogic.getInstance().getCompanies().get(0);
-				comp.setMinerName(usernameText.getText());
-				comp.setDigitalProfit(0);
-				comp.setEmail(emailText.getText());
-				comp.setPassword(passwordText.getText());
-				
-//				String ua;
-//				do {
-//					ua = control.SysData.getInstance().generateRandomStrings(Consts.MINER_ADDRESS_LENGTH);
-//				}
-//				while (control.MinerLogic.getInstance().getMiners().contains(new Miner(ua)));
-				
-				comp.setUniqueAddress(miner.getUniqueAddress());
-				
-				comp.setContactEmail(emailText.getText());
-				comp.setContactFirstName(usernameText1.getText());
-				comp.setContactLastName(usernameText11.getText());
-				comp.setContactPhone(combo.getSelectionModel().getSelectedItem() + "-" + phoneText.getText());
-				
-				control.MinerLogic.getInstance().insertMinerCompany(comp);
-				closeWindow();
-				ViewLogic.newLoginWindow();
-				}
 
-			closeWindow();
-			ViewLogic.newLoginWindow();
+		}else {
+			usernameLable.setVisible(true);
+			usernameLable.setText(("Username is invalid."));
 		}
 	}
 
